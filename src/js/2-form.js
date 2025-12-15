0dac575b2d57040dc21bdbfe833490c5150f873a
Lidiia Rozhko <lida8821@gmail.com>
2025-12-15 21:44:03 +0200

    Add files form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,50 @@
-console.log('Form');
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+populateTextarea();
+
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+  const key = event.target.name;
+  const value = event.target.value.trim();
+
+  formData[key] = value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    const parsedMessage = JSON.parse(savedMessage);
+    formData.email = parsedMessage.email || '';
+    formData.message = parsedMessage.message || '';
+
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData.email = '';
+  formData.message = '';
+  form.reset();
+}
